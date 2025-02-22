fa89005c2098c9ff6991c9a704638a48db02f679
Dhruvi Modha <dhruvimodha1@gmail.com>
2025-02-22 10:27:15 +0530

    project1

--- Project-1/DarkMode/src/App.jsx
+++ Project-1/DarkMode/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
     return (
         <div className={`container ${theme}`}>
+            
+        
             <NavBar theme={theme} setTheme={setTheme} /> 
         </div>
     );
