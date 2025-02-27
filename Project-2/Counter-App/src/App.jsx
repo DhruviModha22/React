03f797fe3ddec8391c275c24e30670d5657be2ac
Dhruvi Modha <dhruvimodha1@gmail.com>
2025-02-27 23:57:11 +0530

    Counter

--- Project-2/Counter-App/src/App.jsx
+++ Project-2/Counter-App/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="container">
       <h1>Welcome to Aesthetic Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{value}</h2>  
       <div>
         <button className="btn" onClick={() => setValue(value + 1)}>Increment</button>
         <button className="btn" disabled={value === 0} onClick={() => setValue(value - 1)}>Decrement</button>
